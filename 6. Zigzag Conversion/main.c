c9ba9d96a5268e0c0f3fa05c255c436cc9648413
mikoogla <78367511+mikoogla@users.noreply.github.com>
2022-10-14 13:24:35 +0200

    format fix

--- 6. Zigzag Conversion/main.c
+++ 6. Zigzag Conversion/main.c
@@ -2,31 +2,32 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct tabsize{
+struct tabsize {
     int rows;
     int cols;
 };
 
-void resetTab(struct tabsize tabsize, char tab[][tabsize.cols]){
-    for(int i=0; i<tabsize.rows; i++){
+void resetTab(struct tabsize tabsize, char tab[][tabsize.cols]) {
+    for (int i = 0; i < tabsize.rows; i++) {
         memset(tab[i], '\0', tabsize.cols);
     }
 }
-char * convert(char * s, int numRows){
-    if(!numRows || numRows <= 0 || s == NULL) return 0;
+
+char *convert(char *s, int numRows) {
+    if (!numRows || numRows <= 0 || s == NULL) return 0;
 
     struct tabsize tabsize = {
-        .rows = numRows,
-        .cols = (int)strlen(s)
+            .rows = numRows,
+            .cols = (int) strlen(s) / 2 + 1
     };
 
     char tab[tabsize.rows][tabsize.cols];
-    resetTab( tabsize, tab);
+    resetTab(tabsize, tab);
     puts("ok");
     return s;
 }
 
 int main() {
     convert("123456789", 3);
-    return 25/2;
+    return 25 / 2;
 }
